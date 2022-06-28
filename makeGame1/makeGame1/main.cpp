811c9660dafe01cc5e22cf11a33866422e2997f7
dami <dami0804@naver.com>
2022-06-28 16:45:04 +0900

    make star

--- makeGame1/makeGame1/main.cpp
+++ makeGame1/makeGame1/main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 #include "Console.h"
 
 
@@ -8,14 +9,29 @@
 |    |  console:(30,30) ¡ã:(14,28)
 |____|
 
+·£´ýÇÔ¼ö srand(time(NULL))    <time.h>
+rand_x = (rand()%15)   <stdlib.h>
+
 */
 int main(void) {
 	int x = 14, y = 28;
 	int bx = 0, by = 0;
 	bool bullet= false;
+
+	int star_x = 0, star_y = 0;
+	bool star = false;
+	
+	srand(time(NULL));
 	SetConsoleSize(30, 30);
 	while (1) {
+
 		Clear();
+		if (!star) {
+			star_x = (rand() % 15) * 2; //0,2,4,6...28
+			star_y = 0;
+			star = true;
+
+		}
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
 			x--;
 			if (x < 0) x = 0;
@@ -38,6 +54,13 @@ int main(void) {
 			printf("¡Ü");
 			by--;
 			if (by < 0)bullet = false;
+		}
+		if (star) {
+			GotoXY(star_x,star_y);
+			printf("¡Ù");
+			star_y++;
+			if (star_y >y)
+				star = false;
 		}
 
 
