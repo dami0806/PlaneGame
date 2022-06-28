8f7922975db567f39b3bf7ea8b4b8896b1c984e9
dami <dami0804@naver.com>
2022-06-28 16:01:26 +0900

    add Shoot

--- makeGame1/makeGame1/main.cpp
+++ makeGame1/makeGame1/main.cpp
@@ -3,26 +3,60 @@
 #include "Console.h"
 
 
+/*
+|----|
+|    |  console:(30,30) ▲:(14,28)
+|____|
+
+*/
 int main(void) {
 	int x = 14, y = 28;
-	//콘솔창 크기조정
+	int bx = 0, by = 0;
+	bool bullet= false;
 	SetConsoleSize(30, 30);
 	while (1) {
 		Clear();
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
 			x--;
-			//화면 넘어가지 않게
 			if (x < 0) x = 0;
-		}if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+		}
+		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
 			x++;
 			if (x > 28) x = 28;
 		}
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+			if (!bullet) {
+				bx = x;
+				by = y - 1;
+				bullet = true;
+			}
 
-		GotoXY(x, y);
+
+		}
+		if (bullet) {
+			GotoXY(bx, by);
+			printf("●");
+			by--;
+			if (by < 0)bullet = false;
+		}
+
+
+		GotoXY(x,y);
 		printf("▲");
-		//Sleep(50);
-
+		Sleep(60);
 	}
 	system("pause");
+
+	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
